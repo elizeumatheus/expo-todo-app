ae3ae09ebcf0ce2f25ad386aefc12ccd32072c94
elizeumatheus <elizeumatheus16@gmail.com>
2021-02-03 22:58:58 -0300

    feat: wrapped the children components with a View and set the StatusBar component with the current the background (android only)

--- src/components/Screen/index.tsx
+++ src/components/Screen/index.tsx
@@ -1,9 +1,20 @@
 import * as React from 'react'
-
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { StatusBar } from 'expo-status-bar'
 
-const Screen: React.FC = ({ children }) => (
-  <SafeAreaView>{children}</SafeAreaView>
-)
+import useTheme from '@/hooks/useTheme'
+
+import * as S from './styles'
+
+const Screen: React.FC = ({ children }) => {
+  const { theme } = useTheme()
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <S.Wrapper>{children}</S.Wrapper>
+      <StatusBar style='auto' backgroundColor={theme.background} />
+    </SafeAreaView>
+  )
+}
 
 export default Screen
---
+++ src/components/Screen/styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components/native'
+
+export const Wrapper = styled.View`
+  background-color: ${({ theme }) => theme.background};
+  flex: 1;
+  padding: 16px;
+`
