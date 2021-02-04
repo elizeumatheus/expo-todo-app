3a2a7eb2e447aec645218b482244371080597edb
elizeumatheus <elizeumatheus16@gmail.com>
2021-02-03 22:59:26 -0300

    feat: set the ThemeProvider from styled-components with the current theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import * as React from 'react'
+import { Text, TouchableOpacity } from 'react-native'
+import { ThemeProvider } from 'styled-components/native'
 import { registerRootComponent } from 'expo'
 
+import Screen from '@/components/Screen'
+import useTheme from '@/hooks/useTheme'
+
 const App = () => {
+  const { theme, toggleTheme } = useTheme()
+
   return (
-    <View>
-      <Text>asdasddspasidjadpsijadspijasdipjas</Text>
-    </View>
+    <ThemeProvider theme={theme}>
+      <Screen>
+        <Text>{theme.background}</Text>
+        <TouchableOpacity onPress={toggleTheme}>
+          <Text>Toggle</Text>
+        </TouchableOpacity>
+      </Screen>
+    </ThemeProvider>
   )
 }
 
