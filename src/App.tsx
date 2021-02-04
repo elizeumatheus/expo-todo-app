c1b241e6621bc1ed790ce3bada48158d3b57177b
elizeumatheus <elizeumatheus16@gmail.com>
2021-02-03 21:41:17 -0300

    chore: moved the entry file to src folder

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import { registerRootComponent } from 'expo'
+
+const App = () => {
+  return (
+    <View>
+      <Text>asdasddspasidjadpsijadspijasdipjas</Text>
+    </View>
+  )
+}
+
+export default registerRootComponent(App)
