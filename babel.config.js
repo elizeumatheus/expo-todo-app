5c32125cf2c4901674cd6cf54b8267434ced2d48
elizeumatheus <elizeumatheus16@gmail.com>
2021-02-03 21:58:47 -0300

    chore: configurated relative path

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,18 @@
-module.exports = function(api) {
-  api.cache(true);
+const path = require('path')
+
+module.exports = function (api) {
+  api.cache(true)
   return {
     presets: ['babel-preset-expo'],
-  };
-};
+    plugins: [
+      [
+        'module-resolver',
+        {
+          alias: {
+            '@': path.join(__dirname, 'src'),
+          },
+        },
+      ],
+    ],
+  }
+}
