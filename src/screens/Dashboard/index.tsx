c66b687a5fadef905a9f75aa8a564c4a9e950bb5
elizeumatheus <elizeumatheus16@gmail.com>
2021-02-03 23:46:17 -0300

    feat: created dashboard screen

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react'
+
+import Screen from '@/components/Screen'
+import Text from '@/components/Text'
+
+const Dashboard = () => (
+  <Screen>
+    <Text size='large' bold>
+      To Do
+    </Text>
+  </Screen>
+)
+
+export default Dashboard
